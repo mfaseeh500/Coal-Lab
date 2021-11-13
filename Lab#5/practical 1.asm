5dd6e2f746f0b2b5a8c63b4dd587d53c399b8526
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-13 09:28:54 +0500

    Create practical 1.asm

---
+++ Lab#5/practical 1.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.data
+
+  v db "Muhammad",0ah,0dh,"$"
+  v1 db ?
+  v2 db "Shahid" ,0ah,0dh,"$"
+ 
+.code ;code start
+main proc
+mov ax, @data
+mov ds,ax  
+;printing of v
+mov dl,offset v   ;can be done with dx as well, here I
+mov ah,09
+int 21h
+;pinting of v1
+mov v2,3 ;can be done with dx as well              
+mov dl,v2
+mov ah,02
+int 21h   
+;printing of v2
+mov dl,offset v2   ;can be done with dx as well
+             mov ah,09
+             int 21h
+         
+         mov ah,4ch
+         int 21h  
+         main endp
+end main
