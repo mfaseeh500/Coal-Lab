9a870df6e986a484b8afdf6cfaf23dec2d0f8d78
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-13 10:22:16 +0500

    Update practical 1.asm

--- Lab#5/practical 1.asm
+++ Lab#5/practical 1.asm
@@ -4,30 +4,33 @@
 
   v db "Muhammad",0ah,0dh,"$"
   v1 db ? 
-  v2 db "Shahid" ,0ah,0dh,"$"
-  newline db 0AH,0DH,"$"
+  v2 db "Faseeh" ,0ah,0dh,"$"
+  newline db ,0AH,0DH,"$"
  
 .code ;code start
 main proc
 mov ax, @data
 mov ds,ax  
 ;printing of v
-mov dl,offset v   ;can be done with dx as well, here I
+mov dl,offset v   ;can be done with dx as well
 mov ah,09
 int 21h
-;pinting of v1
-mov v2,3 ;can be done with dx as well              
-mov dl,v2
+
+
+;printing of v2
+mov dl,offset v2   ;can be done with dx as well
+mov ah,09
+ int 21h   
+ ;printing of newline
+mov dx,offset newline
+mov ah,09
+int 21h  
+ ;pinting of v1
+mov v1,5 ;can be done with dx as well              
+mov dl,v1
 add dl,48
 mov ah,02
 int 21h   
-;printing of v2
-             mov dl,offset newline
-             mov ah,09
-             int 21h
-mov dl,offset v2   ;can be done with dx as well
-mov ah,09
- int 21h
          
          mov ah,4ch
          int 21h  
