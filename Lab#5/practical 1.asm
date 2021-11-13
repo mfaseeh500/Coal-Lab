c0f60ed5fa745fa7bef084a89476e83fde82f565
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-13 10:13:53 +0500

    Update practical 1.asm

--- Lab#5/practical 1.asm
+++ Lab#5/practical 1.asm
@@ -3,8 +3,9 @@
 .data
 
   v db "Muhammad",0ah,0dh,"$"
-  v1 db ?
+  v1 db ? 
   v2 db "Shahid" ,0ah,0dh,"$"
+  newline db 0AH,0DH,"$"
  
 .code ;code start
 main proc
@@ -17,12 +18,16 @@ int 21h
 ;pinting of v1
 mov v2,3 ;can be done with dx as well              
 mov dl,v2
+add dl,48
 mov ah,02
 int 21h   
 ;printing of v2
-mov dl,offset v2   ;can be done with dx as well
+             mov dl,offset newline
              mov ah,09
              int 21h
+mov dl,offset v2   ;can be done with dx as well
+mov ah,09
+ int 21h
          
          mov ah,4ch
          int 21h  
