7fc3b1a7c7022fc9ef9491a6a6a7dbb31b689175
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-20 08:42:02 +0500

    Create small to capital.asm

---
+++ Lab#6/small to capital.asm
@@ -0,0 +1,19 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+    mov ah,1
+    int 21h
+    mov dl,al
+    add dl,32
+    mov ah,02
+    int 21h
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+
+
+
+
