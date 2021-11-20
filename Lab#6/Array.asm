59bac253d6eba3659601e932126836bc9ea3ff6f
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2021-11-20 09:11:35 +0500

    Update Array.asm

--- Lab#6/Array.asm
+++ Lab#6/Array.asm
@@ -1,9 +1,32 @@
-   .model small
+.model small
 .stack 100h
 .data
+arr db 1,2,3,4,5,6,7 
+newline db 0ah,0dh, "$"
 .code
 main proc
-    
+         mov ax,@data
+         mov ds,ax
+       
+         mov cx,7 
+         mov si,0
+             
+            ;printing 1st value
+         mov dl,arr[si]
+         add dl,48   
+         mov ah,02
+        int 21h 
+        ;newline
+        mov dl,offset newline
+        mov ah,09
+        int 21h  
+                   ;printing last value
+         mov dl,arr[si+6]
+         add dl,48   
+         mov ah,02
+        int 21h
+         
+                 
     
     mov ah,4ch
     int 21h
